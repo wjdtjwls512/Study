03bfd2ecd997276c7200044f2d0455600960af41
seojin <jseojin0512@gmail.com>
2026-08-24 23:13:03 +0900

    feat: 자바스크립트 단락 평가 공부

--- js_work/array.js
+++ js_work/array.js
@@ -7,5 +7,4 @@ let arrC = [1, 2, 3, true, "hello", null, undefined, () => {}, {}, []];
 // 2. 배열 요소 접근
 let item1 = arrC[0];
 let item2 = arrC[1];
-
 arrC[0] = "hello";
---
+++ js_work/short-circuit.js
@@ -0,0 +1,21 @@
+// function returnFalse() {
+//   console.log("False 함수");
+//   return undefined;
+// }
+
+// function returnTrue() {
+//   console.log("True 함수");
+//   return 10;
+// }
+
+// console.log(returnFalse() && returnTrue());
+
+// 단락 평가 활용 사례
+
+function printName(person) {
+  const name = person && person.name;
+  console.log(name || "person에 값이 없음");
+}
+
+printName();
+printName({ name: "이정환" });
