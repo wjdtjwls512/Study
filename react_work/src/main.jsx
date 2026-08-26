73288d77b510f09b0e80f34484fa2051a3d8a4eb
seojin <jseojin0512@gmail.com>
2026-08-26 20:34:53 +0900

    chore: 리액트 프로젝트 재설정

--- react_work/src/main.jsx
+++ react_work/src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from './01_component/App.jsx'
-// import App from "./App_ref";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
--- react_work/src/mycounter/src/create_counter/Viewer.jsx
+++
@@ -1,9 +0,0 @@
-function Viewer({count}) {
-  return (
-    <div className="count-box">
-      <h1>{count}</h1>
-    </div>
-  );
-}
-
-export default Viewer;
