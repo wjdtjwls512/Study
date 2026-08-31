07d4f39127ce1118700efaad017ec6f5a2708670
seojin <jseojin0512@gmail.com>
2026-08-31 19:45:44 +0900

    feat: 리액트 Props 공부

---
+++ react_work/src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({children, text, color = "black" }) => {
+  return (
+    <button
+      style={{
+        color: color,
+      }}
+    >
+      {text} - {color.toUpperCase()}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
