d8f98ebfc2e2da5560c4b8358e40a5a21b8bb962
seojin <jseojin0512@gmail.com>
2026-09-08 23:35:02 +0900

    feat: 리액트 useReducer 공부

--- react_todolist_app/src/App.jsx
+++ react_todolist_app/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import Exam from "./components/Exam";
 
 const mockData = [
   {
@@ -59,9 +60,10 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Exam />
+      {/* <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
     </div>
   );
 }
