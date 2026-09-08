a527b54e087737e4f2ae90918bfccae654ff0907
seojin <jseojin0512@gmail.com>
2026-09-08 21:37:52 +0900

    feat: 리액트 투두리스트 앱 렌더링 구현

--- react_todolist_app/src/components/List.jsx
+++ react_todolist_app/src/components/List.jsx
@@ -1,15 +1,35 @@
 import "./List.css";
 import Todoitem from "./Todoitem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilterdData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()));
+  };
+
+  const filterdTodos = getFilterdData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        <Todoitem />
-        <Todoitem />
-        <Todoitem />
+        {filterdTodos.map((todo) => {
+          return <Todoitem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
--- react_todolist_app/src/components/Todoitem.jsx
+++ react_todolist_app/src/components/Todoitem.jsx
@@ -1,11 +1,11 @@
 import "./Todoitem.css";
 
-const Todoitem = () => {
+const Todoitem = ({ id, isDone, content, date }) => {
   return (
     <div className="Todoitem">
       <input type="checkbox" />
-      <div className="content">Todo...</div>
-      <div className="date">Date</div>
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
     </div>
   );
