fd220e580ac93ab085ca6a6fdddb455e4a74594e
seojin <jseojin0512@gmail.com>
2026-09-10 20:34:52 +0900

    feat: 리액트 감정일기장 ui 디자인

--- react_emotion_diary_app/src/App.jsx
+++ react_emotion_diary_app/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -12,10 +13,17 @@ function App() {
   const nav = useNavigate();
 
   const onClickButton = () => {
-    nav("/new")
-  }
+    nav("/new");
+  };
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>new</Link>
--- react_emotion_diary_app/src/main.jsx
+++ react_emotion_diary_app/src/main.jsx
@@ -1,4 +1,5 @@
 import App from "./App.jsx";
+import "./index.css"
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
