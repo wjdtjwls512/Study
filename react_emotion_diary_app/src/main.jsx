074a8d4fd6768a3e1ff156ea225d8a2bf6bdc763
seojin <jseojin0512@gmail.com>
2026-09-10 19:45:21 +0900

    feat: 리액트 감정일기장 앱 라우팅 설정

---
+++ react_emotion_diary_app/src/main.jsx
@@ -0,0 +1,9 @@
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
