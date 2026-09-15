b58b8c2e8d84771653703cc2eb05df54468df1c8
seojin <jseojin0512@gmail.com>
2026-09-15 23:02:25 +0900

    feat: 일기 관리 기능 구현

--- react_emotion_diary_app/src/App.jsx
+++ react_emotion_diary_app/src/App.jsx
@@ -1,54 +1,40 @@
 import "./App.css";
+import { useReducer } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import Button from "./components/Button";
-import Header from "./components/Header";
-import { getEmotionImage } from "./util/get-emotion-image";
+import Edit from "./pages/Edit";
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
   return (
     <>
-      <Header
-        title={"header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log(123);
-        }}
-        type={"DEFAULT"}
-      />
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log(123);
-        }}
-        type={"POSITIVE"}
-      />
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log(123);
-        }}
-        type={"NEGATIVE"}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/Edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
